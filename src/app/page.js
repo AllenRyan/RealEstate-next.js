76cb58aa6887c867960b5c6130bcfec64013634a
AllenRyan <allenisop786@gmail.com>
2024-12-23 10:38:33 +0500

    add header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-   <main className="text-3xl items-center text-red-500">Hello</main>
+   <main className="text-3xl items-center">Hello</main>
   );
 }
